5c75b2838293122283b7b09c4fbcc1a8448fa99f
Jacob Fenton <jsfenfen@gmail.com>
2020-07-20 20:19:30 -0700

    initial commit

---
+++ dumper.py
@@ -0,0 +1,24 @@
+
+import requests
+from datetime import datetime 
+# credentials
+from local_settings import headers
+
+#dry_run = True
+dry_run = False
+
+timestamp = datetime.now()
+
+filestamp = "%s_%s_%s_%s_%s.json" % (timestamp.month, timestamp.day, timestamp.hour, timestamp.minute, timestamp.second)
+print("Writing to file: %s" % filestamp )
+fh = open(filestamp, 'w')
+
+url = "https://adsbexchange-com1.p.rapidapi.com/json/lat/45.5051/lon/-122.6750/dist/10/"
+
+
+
+if not dry_run:
+	response = requests.request("GET", url, headers=headers)
+
+	fh.write(response.text)
+	fh.close()
